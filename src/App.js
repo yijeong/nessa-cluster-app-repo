c3b9501248a96ce113e8a1d4d1d551e01c797170
네사_조이정 <nessa.829@nesa-joijeong-ui-MacBookPro.local>
2022-06-06 15:41:25 +0900

    version update

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog Nessa version 1
+            EKS DEMO Blog Nessa version 2
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
